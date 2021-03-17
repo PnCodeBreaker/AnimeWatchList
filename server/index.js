62715946a30621063dc450d5f609414c4f14fb36
PnCodeBreaker <prithwishnp.n@gmail.com>
2021-03-17 16:44:31 +0530

    setting up initial files

---
+++ server/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended:true }));
+app.use(cors);
+
+const CONNECTION_URL = 'mongodb+srv://pn2001:pnMemories21@cluster0.bficw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then(() => app.listen(PORT, () => console.log(`Server is running on PORT: ${PORT}`)))
+        .catch((error) => console.log(error.message));
+mongoose.set('useFindAndModify', false);
+
