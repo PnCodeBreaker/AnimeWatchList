a6f97af5e64730ffeda1439a554cc4ea3b0cabea
PnCodeBreaker <prithwishnp.n@gmail.com>
2021-03-23 13:01:28 +0530

    adding material ui styles

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,17 @@ import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
 import memories from './images/memories.png';
 
+import useStyles from './styles';
+
 const App = () => {
+
+    const classes = useStyles();
+
     return (
         <Container maxwidth = "lg">
-            <AppBar position="static" color="inherit">
-            <Typography variant="h2" align="center">Memories</Typography>
-            <img src={memories} alt="memories" height="60" />
+            <AppBar className={classes.appBar} position="static" color="inherit">
+            <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
+            <img className={classes.image} src={memories} alt="memories" height="60"/>
             </AppBar>
             <Grow in>
                 <Container>
