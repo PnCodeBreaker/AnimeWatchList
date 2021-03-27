94f04e9657fde3f8211923044fbeb16f5641c675
PnCodeBreaker <prithwishnp.n@gmail.com>
2021-03-27 16:17:59 +0530

    front end form ui complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
+
+import { getPosts } from './actions/posts';
 
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
@@ -10,6 +13,11 @@ import useStyles from './styles';
 const App = () => {
 
     const classes = useStyles();
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getPosts());
+    },[dispatch]);
 
     return (
         <Container maxwidth = "lg">
--- server/index.js
+++ server/index.js
@@ -7,11 +7,13 @@ import postRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use('/posts', postRoutes);
+
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended:true }));
-app.use(cors);
+app.use(cors());
+
+app.use('/posts', postRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://pn2001:pnMemories21@cluster0.bficw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000;
